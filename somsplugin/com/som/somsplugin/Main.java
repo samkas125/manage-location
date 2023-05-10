7f44981f178962f6223dd383359176224fb52dda
Samtxx <sompokefan@gmail.com>
2023-05-10 22:37:45 +0530

    cleanup

--- somsplugin/com/som/somsplugin/Main.java
+++ somsplugin/com/som/somsplugin/Main.java
@@ -1,35 +1,26 @@
-/*    */ package com.som.somsplugin;
-/*    */ 
-/*    */ import java.io.PrintStream;
-/*    */ import org.bukkit.command.PluginCommand;
-/*    */ import org.bukkit.configuration.file.FileConfiguration;
-/*    */ import org.bukkit.configuration.file.FileConfigurationOptions;
-/*    */ import org.bukkit.event.Listener;
-/*    */ import org.bukkit.plugin.java.JavaPlugin;
-/*    */ 
-/*    */ public class Main extends JavaPlugin
-/*    */   implements Listener
-/*    */ {
-/*    */   public static Main plugin;
-/*    */ 
-/*    */   public void onEnable()
-/*    */   {
-/* 12 */     System.out.println("PLUGIN HAS BEEN ENABLED!");
-/* 13 */     getConfig().options().copyDefaults();
-/* 14 */     saveDefaultConfig();
-/* 15 */     getCommand("setloc").setExecutor(new setlocationcommand());
-/* 16 */     getCommand("gotoloc").setExecutor(new gotolocationcommand());
-/* 17 */     getCommand("clearloc").setExecutor(new clearlocationcommand());
-/* 18 */     plugin = this;
-/*    */   }
-/*    */ 
-/*    */   public void onDisable()
-/*    */   {
-/* 25 */     System.out.println("PLUGIN HAS BEEN DISABLED!");
-/*    */   }
-/*    */ }
+package com.som.somsplugin;
 
-/* Location:           D:\sompo\somsplugin.jar
- * Qualified Name:     com.som.somsplugin.Main
- * JD-Core Version:    0.6.2
- */
+import java.io.PrintStream;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.FileConfigurationOptions;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+ 
+ public class Main extends JavaPlugin implements Listener {
+   public static Main plugin;
+ 
+   public void onEnable() {
+     System.out.println("PLUGIN HAS BEEN ENABLED!");
+     getConfig().options().copyDefaults();
+     saveDefaultConfig();
+     getCommand("setloc").setExecutor(new setlocationcommand());
+     getCommand("gotoloc").setExecutor(new gotolocationcommand());
+     getCommand("clearloc").setExecutor(new clearlocationcommand());
+     plugin = this;
+   }
+ 
+   public void onDisable() {
+     System.out.println("PLUGIN HAS BEEN DISABLED!");
+   }
+ }
